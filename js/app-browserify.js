1b1ffc95c01510daaff8c615e689a4d1f8fa0fab
Matthew Keas <matthiasak@gmail.com>
2015-06-08 17:01:21 -0500

    initial commit

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -2,25 +2,66 @@
 
 // es5 polyfills, powered by es5-shim
 require("es5-shim")
+
 // es6 polyfills, powered by babel
 require("babel/register")
-
+var backbone = require('backbone')
 var Promise = require('es6-promise').Promise
-// just Node?
-// var fetch = require('node-fetch')
-// Browserify?
-// require('whatwg-fetch') //--> not a typo, don't store as a var
 
-// other stuff that we don't really use in our own code
-// var Pace = require("../bower_components/pace/pace.js")
+window.backbone = backbone
+window.$ = backbone.$
+window._ = require("underscore")
 
-// require your own libraries, too!
-// var Router = require('./app.js')
+var key = `d353c94884828ab143c8633437f899aa`
 
-// window.addEventListener('load', app)
+window.Forecast = backbone.Model.extend({
+    defaults: {
+        key: key
+    },
 
-// function app() {
-    // start app
-    // new Router()
-// }
+    validate: function(attrs) {
+        if (!attrs.lat || !attrs.lng) return "No lat/lng provided."
+    },
+
+    initialize: function(){
+        this.on("request", () => {
+            console.log('requesting data')
+        })
+        this.on("sync", () => {
+            console.log('request finished')
+        })
+        this.on("error", (...args) => {
+            console.error(args)
+        })
+    },
+
+    urlRoot: function(){
+        return `https://api.forecast.io/forecast/${this.get('key')}/${this.get('lat')},${this.get('lng')}?callback=?`
+    }
+})
+
+var forecast_html = (forecastJSON) => `<div>
+    <p>${forecastJSON.currently.temperature}</p>
+    <span>${new Date().toLocaleTimeString()}</span>
+</div>`
+
+var f = new Forecast({lat:26, lng: -90})
+
+f.on('sync', function(model){
+    document.body.innerHTML = forecast_html(model.toJSON())
+})
+
+setInterval(() => {
+    f.fetch()
+}, 5*1000)
+
+f.fetch()
+
+
+
+
+
+
+
+
 
